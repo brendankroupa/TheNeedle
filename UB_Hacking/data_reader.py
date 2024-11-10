82f473dce51c1ffaa36fc3b293a5e7da83c0e7eb
carterga <carterga@buffalo.edu>
2024-11-10 01:06:08 -0500

    Update data_reader.py

--- UB_Hacking/data_reader.py
+++ UB_Hacking/data_reader.py
@@ -1,7 +1,7 @@
 import csv
 
 def read_data():
-    with open("UB_Hacking/bias_weights.csv", newline="") as csvfile:
+    with open("C:\\Users\\4051c\\code\\python\\the-needle\\ub-hacking-create-your-repo-here-theneedle-main\\UB_Hacking\\bias_weights.csv", newline="") as csvfile:
         data = csv.reader(csvfile, delimiter=",")
-        data_dict = {row[0]: row[1] for row in data}
+        data_dict = {row[0].lower(): int(row[1]) for row in data}
         return data_dict
